8dd020f6958b14be5080d33eee26696c81775542
Walter V. Faustine <waltervfaustine@gmail.com>
2019-06-19 15:08:15 +0300

    feat(components): added menu item components

---
+++ src/app/container/maintenance-forms/maintenance-forms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance-forms',
+  templateUrl: './maintenance-forms.component.html',
+  styleUrls: ['./maintenance-forms.component.scss']
+})
+export class MaintenanceFormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/container/maintenance-orgunits/maintenance-orgunits.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance-orgunits',
+  templateUrl: './maintenance-orgunits.component.html',
+  styleUrls: ['./maintenance-orgunits.component.scss']
+})
+export class MaintenanceOrgunitsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
