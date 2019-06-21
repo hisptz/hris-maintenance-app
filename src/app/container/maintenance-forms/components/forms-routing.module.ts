7d142432f3f0c78f1610ea2af6f63706dfc40226
Walter V. Faustine <waltervfaustine@gmail.com>
2019-06-21 16:28:09 +0300

    feat(forms-component): added support to form component

---
+++ src/app/container/maintenance-forms/components/forms-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsComponent } from './forms/forms.component';
+
+const routes: Routes = [
+  {
+    path: 'filters',
+    component: FormsComponent,
+    children: [
+      {
+        path: ':name',
+        component: FormsComponent,
+      }
+    ],
+  },
+];
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class FormsRoutingModule { }
