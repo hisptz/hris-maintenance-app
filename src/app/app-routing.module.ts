c6cd524f85e49abd5c97b3888eea0cd219b0af72
Walter V. Faustine <waltervfaustine@gmail.com>
2019-06-21 12:31:18 +0300

    fix(app-routing.module.ts): added routes for the menu bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AllMaintenanceOptionsComponent } from './container/all-maintenance-options/all-maintenance-options.component';
+import { MaintenanceFieldsComponent } from './container/maintenance-fields/maintenance-fields.component';
+import { MaintenanceFiltersComponent } from './container/maintenance-filters/maintenance-filters.component';
+import { MaintenanceFormsComponent } from './container/maintenance-forms/maintenance-forms.component';
+import { MaintenanceOrgunitsComponent } from './container/maintenance-orgunits/maintenance-orgunits.component';
+import { MaintenanceOthersComponent } from './container/maintenance-others/maintenance-others.component';
+import { FieldsComponent } from './container/maintenance-fields/components/fields/fields.component';
+
+const routes: Routes = [
+  { path: 'all', component: AllMaintenanceOptionsComponent },
+  { path: 'fields', component: MaintenanceFieldsComponent },
+  { path: 'filters', component: MaintenanceFiltersComponent },
+  { path: 'forms', component: MaintenanceFormsComponent },
+  { path: 'organizationUnits', component: MaintenanceOrgunitsComponent },
+  { path: 'others', component: MaintenanceOthersComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { enableTracing: false })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
