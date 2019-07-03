edfc44f29146d623352050c6013510afda703614
Walter V. Faustine <waltervfaustine@gmail.com>
2019-07-03 17:02:49 +0300

    refactor: changing code alignment

--- src/app/shared/components/menu-aside/menu-aside.component.ts
+++ src/app/shared/components/menu-aside/menu-aside.component.ts
@@ -13,15 +13,21 @@ export class MenuAsideComponent implements OnInit {
   @Input() menuSettings;
   rippleEffectsColor: string;
 
-  menuOptions: MenuOptions;
+  menuOptions: Array<MenuOptions>;
+  isListOpened = false;
+  isServiceOpened = false;
 
   constructor() {}
 
   ngOnInit() {
     this.rippleEffectsColor = '#ccc';
+    this.isListOpened = false;
+    this.isServiceOpened = true;
   }
 
-  openMenuItemsList = (options: MenuOptions) => {
+  openMenuItemsList = (options: Array<MenuOptions>) => {
     this.menuOptions = options;
+    this.isListOpened = true;
+    this.isServiceOpened = false;
   }
 }
--- src/app/shared/components/menu-containers/menu-containers.component.ts
+++ src/app/shared/components/menu-containers/menu-containers.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuOptions } from '../../models/menu-configuration';
+import { Component, OnInit, Input } from '@angular/core';
+import { MenuOptions, ItemList } from '../../models/menu-configuration';
 
 @Component({
   selector: 'app-menu-containers',
