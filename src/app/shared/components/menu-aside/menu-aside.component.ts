f1beb5eb9981b023960843ac330634b48c4c1bf4
Walter V. Faustine <waltervfaustine@gmail.com>
2019-07-04 18:03:12 +0300

    fix(fields): added support to load data via services

--- src/app/shared/components/menu-aside/menu-aside.component.ts
+++ src/app/shared/components/menu-aside/menu-aside.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {
-  MenuConfiguration,
-  MenuOptions,
-} from '../../../models/menu-configuration';
+import { MenuOptions } from '../../../models/menu-configuration';
+import { Fields } from 'src/app/models/fields.model';
+import { FieldsService } from 'src/app/pages/services/fields.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-menu-aside',
@@ -10,24 +12,45 @@ import {
   styleUrls: ['./menu-aside.component.scss'],
 })
 export class MenuAsideComponent implements OnInit {
-  @Input() menuSettings;
+  fieldsSettings: Array<Fields>;
   rippleEffectsColor: string;
 
   menuOptions: Array<MenuOptions>;
+  errorMessage: ErrorMessage;
   isListOpened = false;
   isServiceOpened = false;
 
-  constructor() {}
+  constructor(
+    private fieldsService: FieldsService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.rippleEffectsColor = '#ccc';
     this.isListOpened = false;
     this.isServiceOpened = true;
+    this.getFields();
   }
 
   openMenuItemsList = (options: Array<MenuOptions>) => {
     this.menuOptions = options;
     this.isListOpened = true;
     this.isServiceOpened = false;
+  }
+
+  getFields = () => {
+    const fields$ = this.fieldsService.getAllTheFields();
+    this.fieldsService.getAllTheFields().subscribe(
+      (fields: Array<Fields>) => {
+        const fieldsData = _.filter(fields, (field: Fields) => {
+          return field.route === _.last(_.split(this.router.url, '/'));
+        });
+        fieldsData ? (this.fieldsSettings = fieldsData) : [];
+      },
+      (error: ErrorMessage) => {
+        this.errorMessage = error;
+        console.error();
+      }
+    );
   }
 }
