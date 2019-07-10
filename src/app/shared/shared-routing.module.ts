df74ffedd192ca1899b0062f706a811975ebb225
Walter V. Faustine <waltervfaustine@gmail.com>
2019-07-10 12:45:00 +0300

    feat(progress-bar): added support for the progress bar

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -1,22 +1,8 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AllMaintenanceOptionsComponent } from '../container/all-maintenance-options/all-maintenance-options.component';
-import { FieldsComponent } from '../pages/components/fields/fields.component';
-import { ReportsComponent } from '../pages/components/reports/reports.component';
-import { FormsComponent } from '../pages/components/forms/forms.component';
-import { OrgunitsComponent } from '../pages/components/orgunits/orgunits.component';
-import { OthersComponent } from '../pages/components/others/others.component';
+import { RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'all', component: AllMaintenanceOptionsComponent },
-  { path: 'fields', component: FieldsComponent },
-  { path: 'reports', component: ReportsComponent },
-  { path: 'forms', component: FormsComponent },
-  { path: 'organizationUnits', component: OrgunitsComponent },
-  { path: 'others', component: OthersComponent },
-];
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild([])],
   exports: [RouterModule],
 })
-export class SharedRoutingModule { }
+export class SharedRoutingModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,33 +6,55 @@ import { MenuServicesComponent } from './components/menu-services/menu-services.
 // Import: Angular Materials Dependencies
 import { MatRippleModule } from '@angular/material/core';
 import { SharedRoutingModule } from './shared-routing.module';
-import { MenuAsideComponent } from './components/menu-aside/menu-aside.component';
+// import { MenuAsideComponent } from './components/menu-aside/menu-aside.component';
 import { MenuContainersComponent } from './components/menu-containers/menu-containers.component';
 import { ListViewComponent } from './components/list-view/list-view.component';
 
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material';
+// import { ContentAreaComponent } from './components/content-area/content-area.component';
+// import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { MainContentAreaComponent } from './components/main-content-area/main-content-area.component';
+import { MainContentContainerComponent } from './components/main-content-container/main-content-container.component';
+import { MainSideMenuComponent } from './components/main-side-menu/main-side-menu.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { ProgressLoaderComponent } from './components/progress-loader/progress-loader.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
     MenuHeaderComponent,
     MenuServicesComponent,
-    MenuAsideComponent,
+    // MenuAsideComponent,
     MenuContainersComponent,
     ListViewComponent,
+    // ContentAreaComponent,
+    // SideMenuComponent,
+    MainContentAreaComponent,
+    MainContentContainerComponent,
+    MainSideMenuComponent,
+    ProgressLoaderComponent,
+    ProgressBarComponent,
   ],
   imports: [
     CommonModule,
+    MatMenuModule,
     MatRippleModule,
     SharedRoutingModule,
     MatTableModule,
     MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule
   ],
   exports: [
     MenuHeaderComponent,
     MenuServicesComponent,
-    MenuAsideComponent,
+    // MenuAsideComponent,
     ListViewComponent,
+    MainContentContainerComponent,
+    // ProgressBarComponent,
   ],
 })
 export class SharedModule {}
