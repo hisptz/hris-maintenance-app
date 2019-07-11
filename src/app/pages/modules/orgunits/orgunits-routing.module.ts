1491bdd24757d234e968f9c2d3fb87c4f774fd66
Walter V. Faustine <waltervfaustine@gmail.com>
2019-07-11 11:56:20 +0300

    refactor(orgunits-module): refactoring orgunits module

--- src/app/pages/modules/orgunits/orgunits-routing.module.ts
+++ src/app/pages/modules/orgunits/orgunits-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrgunitsComponent } from './orgunits.component';
+import { MainContentAreaComponent } from 'src/app/shared/components/main-content-area/main-content-area.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: 'organizationUnits',
+    component: OrgunitsComponent,
+    children: [
+      {
+        path: ':name',
+        component: MainContentAreaComponent,
+      },
+    ],
+  },
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
