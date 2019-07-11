77c742e7c790e2d681ee07ad9395679e85cb96ab
Walter V. Faustine <waltervfaustine@gmail.com>
2019-07-11 21:38:59 +0300

    fix(home-module): added home all services
    All the services available for the Maintenance App are all put on one single page for the easy
    access

--- src/app/pages/modules/home/home.component.ts
+++ src/app/pages/modules/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MenuSetting } from 'src/app/models/menu-setting.model';
+import { Router } from '@angular/router';
+
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  homeSettings: Array<MenuSetting>;
+  errorMessage: ErrorMessage;
+  rippleEffectsColor: string;
+  routerNavigation: string;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.rippleEffectsColor = '#ccc';
+    this.routerNavigation = _.last(_.split(this.router.url, '/'));
   }
-
 }
