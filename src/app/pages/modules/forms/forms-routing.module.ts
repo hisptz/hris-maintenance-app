d41bede22dd629558391ad1e465465bff52a74e0
Walter V. Faustine <waltervfaustine@gmail.com>
2019-07-11 11:39:02 +0300

    refactor(forms-module): refactoring the forms modules

--- src/app/pages/modules/forms/forms-routing.module.ts
+++ src/app/pages/modules/forms/forms-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsComponent } from './forms.component';
+import { MainContentAreaComponent } from 'src/app/shared/components/main-content-area/main-content-area.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: 'forms',
+    component: FormsComponent,
+    children: [
+      {
+        path: ':name',
+        component: MainContentAreaComponent,
+      },
+    ],
+  },
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
