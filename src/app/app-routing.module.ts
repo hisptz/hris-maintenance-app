4fa8777ba7a3be7fbd0124062424031174e401e4
Walter V. Faustine <waltervfaustine@gmail.com>
2019-11-18 15:42:37 +0300

    feat: 🎸 upgraded project angular version from v7 to angular v8
    This commit will upgrade the version of angular from angular version 7
    to angular version 8
    
    BREAKING CHANGE: Project Angular version changed from angular v7 to angular v8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
-
 const routes: Routes = [];
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
       useHash: true,
       preloadingStrategy: PreloadAllModules,
-      enableTracing: false
+      enableTracing: false,
     }),
   ],
   exports: [RouterModule],
