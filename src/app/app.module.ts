dd7b836310f3aad48c0c4a7a02f9cb755ea85530
Walter V. Faustine <waltervfaustine@gmail.com>
2019-11-18 15:04:27 +0300

    refactor: 💡 removed all components and re-written again
    This commit will remove all components and re-write all of them again in
    a standard way

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
-import { FieldsModule } from './pages/modules/fields/fields.module';
-import { ReportsModule } from './pages/modules/reports/reports.module';
-import { FormsModule } from './pages/modules/forms/forms.module';
-import { OthersModule } from './pages/modules/others/others.module';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
@@ -30,10 +26,6 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     HttpClientModule,
     SharedModule,
     PagesModule,
-    FieldsModule,
-    ReportsModule,
-    FormsModule,
-    OthersModule,
     AppRoutingModule,
     MatToolbarModule,
     NgbPaginationModule,
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,26 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MatRippleModule } from '@angular/material';
 import { PagesRoutingModule } from './pages-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { ReportsComponent } from './modules/reports/reports.component';
-import { FieldsComponent } from './modules/fields/fields.component';
-import { FormsComponent } from './modules/forms/forms.component';
-import { OrgunitsComponent } from './modules/orgunits/orgunits.component';
-import { OthersComponent } from './modules/others/others.component';
-import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
-  declarations: [
-    ReportsComponent,
-    FieldsComponent,
-    FormsComponent,
-    OrgunitsComponent,
-    OthersComponent,
-    HomeComponent
-    // MenuContainerComponent,
-  ],
+  declarations: [],
   imports: [CommonModule, SharedModule, PagesRoutingModule, MatRippleModule],
-  exports: [
-    // MenuContainerComponent
-  ],
+  exports: [],
 })
 export class PagesModule {}
