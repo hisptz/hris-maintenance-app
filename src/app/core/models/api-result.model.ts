cc42bc9dc58922c79681a54ee2d015831b5892fe
Walter V. Faustine <waltervfaustine@gmail.com>
2019-11-21 16:13:36 +0300

    fix: 🐛 added api-result model
    This commit will add api results model

---
+++ src/app/core/models/api-result.model.ts
@@ -0,0 +1,32 @@
+export interface APIResult {
+    pager: Pager;
+    fields: Field[];
+}
+
+export interface Pager {
+    page: number;
+    pageSize: number;
+    pageCount: number;
+    total: number;
+    nextPage: string;
+}
+
+export interface Field {
+    created: Date;
+    lastUpdated: Date;
+    id: number;
+    uid: string;
+    code?: any;
+    name: string;
+    caption: string;
+    compulsory?: any;
+    isUnique?: any;
+    isCalculated?: any;
+    description?: any;
+    calculatedExpression?: any;
+    hasHistory?: any;
+    hasOptions: boolean;
+    hasTarget?: any;
+    fieldRelation?: any;
+    skipInReport?: any;
+}
